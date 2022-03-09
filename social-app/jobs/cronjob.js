771ea651de5ca484f0b0420c960ada870879252d
Xeragus <bobansugareski@gmail.com>
2022-03-09 22:02:39 +0100

    Fetch posts in cronjob

--- social-app/jobs/cronjob.js
+++ social-app/jobs/cronjob.js
@@ -1,5 +1,14 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob('*/10 * * * * *', () => {
-  console.log('You will see this message every 10 seconds');
+const CronJob = require('cron').CronJob;
+const Post = require('../models/post');
+
+const job = new CronJob('*/5 * * * * *', async () => {
+  const d = new Date();
+  d.setMinutes(d.getMinutes() - 5);
+  const posts = await Post.find({createdAt: {$gte: d}});
+
+  /** Send mail */
+
+  console.log(posts);
 });
+
 job.start();
